372e50e82c87afe2a8ce5f36097d40ba8b747cad
Jefferson <jtsalim04@gmail,com>
2026-05-21 21:57:15 +0800

    style: clean up API integration formatting

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,6 +4,13 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
+
+jest.mock('../src/navigations', () => {
+  const ReactMock = require('react');
+  const { View } = require('react-native');
+  return () => ReactMock.createElement(View, { testID: 'app-nav' });
+});
+
 import App from '../App';
 
 test('renders correctly', async () => {
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  setupFiles: [
+    require.resolve('@react-native/jest-preset/jest/setup.js'),
+    '<rootDir>/jest/setup.js',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
+      '@react-native/jest-preset/jest/assetFileTransformer.js',
+    ),
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-redux|redux-persist|@react-native-async-storage)/)',
+  ],
 };
